1b6917ab63148d03b4ecf56d27e9a688f3810fc0
881and886 <881and886@gmail.com>
2026-05-26 12:32:15 +0800

    外圈＋八大內圈系統修正

---
+++ src/data/innerMaps.js
@@ -0,0 +1,12 @@
+export const innerMaps = {
+  academy: {
+    spaces: [
+      '入學',
+      '選課',
+      '社團',
+      '報告',
+      '期中考',
+      '畢業'
+    ]
+  }
+}
---
+++ src/data/outerMap.js
@@ -0,0 +1,6 @@
+export const outerMap = [
+  { id: 0, name: '起點' },
+  { id: 1, name: '工作' },
+  { id: 2, name: '投資' },
+  { id: 3, name: '學院入口', route: 'academy' },
+]
